5512975895ea3518ff2b9763dfa4792d401035de
bonraizen <subektifajar82@gmail.com>
2025-04-03 22:37:53 +0700

    initial commit

---
+++ app/src/main/java/com/example/alber2/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.alber2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button pindah;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+            pindah = findViewById(R.id.startButton);
+
+            pindah.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent pindahsignup = new Intent(MainActivity.this,SignUp.class);
+                    startActivity(pindahsignup);
+                }
+            });
+    }
+}
