16ce10a207fa3f7a1871520dcc3a18ad3a0bdd5c
bonraizen <subektifajar82@gmail.com>
2025-04-08 00:31:18 +0700

    initial commit

---
+++ app/src/main/java/com/example/alber2/beranda.java
@@ -0,0 +1,49 @@
+package com.example.alber2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class beranda extends AppCompatActivity {
+
+    ImageButton btnexca,btnroller,btndozer,btnbreakerexca,btnforklift,btnlift;
+
+
+    public void exca  () {
+        Intent pindahexa = new Intent(beranda.this,bucketexca.class);
+        startActivity(pindahexa);
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_beranda);
+
+        btnexca = findViewById(R.id.btnexca);
+        btnroller = findViewById(R.id.btnroller);
+        btndozer = findViewById(R.id.btndozer);
+        btnbreakerexca = findViewById(R.id.btnbreakerexca);
+        btnforklift = findViewById(R.id.btnforklift);
+        btnlift = findViewById(R.id.btnlift);
+
+
+
+        btnexca.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                exca();
+            }
+        });
+
+
+
+    }
+}
