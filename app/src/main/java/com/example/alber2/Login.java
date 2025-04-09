0317ec0e882efcab2f74c14e9fae5cb2ae158838
bonraizen <subektifajar82@gmail.com>
2025-04-10 01:41:57 +0700

    initial commit

--- app/src/main/java/com/example/alber2/Login.java
+++ app/src/main/java/com/example/alber2/Login.java
@@ -4,16 +4,36 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class Login extends AppCompatActivity {
 
-    Button pindah,signin;
+    private DatabaseReference database;
+    Button pindah;
+
+    private EditText email_input,password_input;
+    private Button signin;
+
+    public void masukan(){
+        pindah = findViewById(R.id.btnsignup);
+        signin = findViewById(R.id.sign_in_button);
+        email_input = findViewById(R.id.email_input);
+        password_input = findViewById(R.id.email_input);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +41,7 @@ public class Login extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
 
-
-        pindah = findViewById(R.id.btnsignup);
-        signin = findViewById(R.id.sign_in_button);
+        masukan();
 
         pindah.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,8 +54,33 @@ public class Login extends AppCompatActivity {
         signin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent signini = new Intent(Login.this,beranda.class);
-                startActivity(signini);
+                String email = email_input.getText().toString();
+                String password = password_input.getText().toString();
+
+                database = FirebaseDatabase.getInstance().getReference("user");
+
+                if (email.isEmpty()||password.isEmpty()){
+                    Toast.makeText(Login.this, "Email dan Password harus Di ISI", Toast.LENGTH_SHORT).show();
+                }else {
+                    database.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            if(snapshot.child(email).exists()){
+                                if(snapshot.child(email).child("password").getValue(String.class).equals(password)){
+                                    Intent signini = new Intent(Login.this,beranda.class);
+                                    startActivity(signini);
+                                }
+                            }else{
+                                Toast.makeText(Login.this, "Email belum terdaftar atau Password salah", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                }
             }
         });
     }
--- app/src/main/java/com/example/alber2/SignUp.java
+++ app/src/main/java/com/example/alber2/SignUp.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,12 +13,30 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class SignUp extends AppCompatActivity {
 
+    private DatabaseReference database;
+
     Button btnlogin;
+    private Button btnsignup;
 
+    private EditText etnama,etnamaPerusahaan,etnomorTelepon,etemail,etnikKTP,etcreatePassword,etconfirmPassword;
 
+    public void sumber(){
+        btnlogin = findViewById(R.id.btnlogin);
+        btnsignup =findViewById(R.id.btnsignup);
+        etnama = findViewById(R.id.etnama);
+        etnamaPerusahaan = findViewById(R.id.etnamaPerusahaan);
+        etnomorTelepon = findViewById(R.id.etnomorTelepon);
+        etemail = findViewById(R.id.etemail);
+        etnikKTP = findViewById(R.id.etnikKTP);
+        etcreatePassword = findViewById(R.id.etcreatePassword);
+        etconfirmPassword = findViewById(R.id.etconfirmPassword);
+    }
     @Override
     protected void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -28,13 +48,46 @@ public class SignUp extends AppCompatActivity {
             return insets;
         });
 
-        btnlogin = findViewById(R.id.btnlogin);
+        database = FirebaseDatabase.getInstance().getReferenceFromUrl("https://sewa-alber-default-rtdb.firebaseio.com/");
+
+        sumber();
 
         btnlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent pindahlogin = new Intent(SignUp.this,Login.class);
                 startActivity(pindahlogin);
+            }
+        });
+
+        btnsignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nama = etnama.getText().toString();
+                String namaPerusahaan = etnamaPerusahaan.getText().toString();
+                String nomorTelepon = etnomorTelepon.getText().toString();
+                String email = etemail.getText().toString();
+                String nikKTP = etnikKTP.getText().toString();
+                String createPassword = etcreatePassword.getText().toString();
+                String confirmPassword = etconfirmPassword.getText().toString();
+
+                if(nama.isEmpty()||namaPerusahaan.isEmpty()||nomorTelepon.isEmpty()||email.isEmpty()||nikKTP.isEmpty()||createPassword.isEmpty()||confirmPassword.isEmpty()) {
+                    Toast.makeText(SignUp.this, "Tidak boleh ada form yang kosong", Toast.LENGTH_SHORT).show();
+
+                }else{
+                        database = FirebaseDatabase.getInstance().getReference("user");
+                        database.child(email).child("nama").setValue(nama);
+                        database.child(email).child("namaPerusahaan").setValue(namaPerusahaan);
+                        database.child(email).child("nomorTelepon").setValue(nomorTelepon);
+                        database.child(email).child("email").setValue(email);
+                        database.child(email).child("nikKTP").setValue(nikKTP);
+                        database.child(email).child("confirmPassword").setValue(confirmPassword);
+                        Toast.makeText(SignUp.this, "Data Berhasil Disimpan", Toast.LENGTH_SHORT).show();
+
+                        Intent login = new Intent(SignUp.this,Login.class);
+                        startActivity(login);
+                    }
+
             }
         });
     }
