1e88734f3d0f3bf6841f492dd01d9d2b6527573e
bonraizen <subektifajar82@gmail.com>
2025-05-09 23:33:18 +0700

    initial commit

--- app/src/main/java/com/example/alber2/roller.java
+++ app/src/main/java/com/example/alber2/roller.java
@@ -23,15 +23,6 @@ public class roller extends AppCompatActivity {
         binding = ActivityRollerBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_roller);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
 }
