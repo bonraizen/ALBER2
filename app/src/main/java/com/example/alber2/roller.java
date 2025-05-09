ecec931d9b9e1397a9735c9cf9d2548e5a8efbac
bonraizen <subektifajar82@gmail.com>
2025-05-10 03:22:40 +0700

    initial commit

--- app/src/main/java/com/example/alber2/roller.java
+++ app/src/main/java/com/example/alber2/roller.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -14,14 +15,13 @@ import com.example.alber2.databinding.ActivityRollerBinding;
 
 public class roller extends AppCompatActivity {
 
-    private ActivityRollerBinding binding;
 
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_roller);
 
-        binding = ActivityRollerBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
 
     }
 
